feb3540da760570c7cfd5b145bca5f7beea4cfb4
pedram <pedramnaghib@gmail.com>
2023-04-20 18:41:17 +0300

    Fix run.py

--- ToDo.md
+++
@@ -1,5 +0,0 @@
-ToDos:
-
-* complete roulette
-* fix run.py
-* add send_all function so that admin can send message to all users
--- run.py
+++ run.py
@@ -6,8 +6,8 @@ import os
 if __name__ == "__main__":
     # if you want to run this bot on your system rather than on a hosting service
     # comment out next 2 lines and uncooment last two lines
-    server.run(host="0.0.0.0", port=8080)
-    logger.info("Bot Started")
+    # server.run(host="0.0.0.0", port=8080)
+    # logger.info("Bot Started")
 
-    # import src.responses
-    # bot.infinity_polling()
+    import src.responses
+    bot.infinity_polling()
